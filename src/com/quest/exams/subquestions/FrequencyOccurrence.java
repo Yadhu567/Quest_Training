3133e9a05e66d2cd11a38f3b438e6490321790ab
Yadhu567 <yadhukrishnanrma@gmail.com>
2024-12-03 17:48:40 +0530

    Day 20 exam sub questions

--- src/com/quest/exams/subquestions/FrequencyOccurrence.java
+++ src/com/quest/exams/subquestions/FrequencyOccurrence.java
@@ -4,39 +4,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class FrequencyOccurrence {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arrayList=new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(3);
-        arrayList.add(3);
+        System.out.println("Enter the number of elements in the list:");
+        int numberOfElements = sc.nextInt();
 
-        Map<Integer,List<Integer>> resultMap;
-        resultMap= groupByFrequency(arrayList); // stores the result map
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < numberOfElements; i++) {
+            arrayList.add(sc.nextInt());
+        }
+
+        System.out.println("The input array list");
+        System.out.println(arrayList);
+
+        Map<Integer,List<Integer>> resultMap= groupByFrequency(arrayList); // stores the result map
+
+        System.out.println("The resultant map is");
         System.out.println(resultMap);
     }
     public static Map<Integer, List<Integer>>  groupByFrequency(ArrayList<Integer> arrayList){
-            Map<Integer,Integer> frequencyMap=new HashMap<>();
+            Map<Integer,Integer> frequencyMap=new HashMap<>(); // map to store the elements and their frequency
 
             for(Integer num :arrayList){
-                frequencyMap.put(num,frequencyMap.getOrDefault(num,0)+1); // go count the frequency of occurrence of element
+                frequencyMap.put(num,frequencyMap.getOrDefault(num,0)+1); // to count the frequency of occurrence of element
             }
 
-
-            Map<Integer,List<Integer>> groupByFrequencyMap=new HashMap<>(); // storing the result
+            Map<Integer,List<Integer>> groupByFrequencyMap=new HashMap<>(); // storing the resultant map
 
             for(Map.Entry<Integer,Integer> entry:frequencyMap.entrySet()){
-                int number= entry.getKey();
+                int element= entry.getKey();
                 int frequency=entry.getValue();
 
                 if (!groupByFrequencyMap.containsKey(frequency)) {
-                    groupByFrequencyMap.put(frequency, new ArrayList<>()); // creating new arraylist for new key
+                    groupByFrequencyMap.put(frequency,new ArrayList<>()); // creating new arraylist for new key
                 }
-                groupByFrequencyMap.get(frequency).add(number); //fetch the list of key and add to that
+                groupByFrequencyMap.get(frequency).add(element); //fetch the list of key and add to that
             }
             return  groupByFrequencyMap;
     }
--- src/com/quest/exams/subquestions/ListPartition.java
+++ src/com/quest/exams/subquestions/ListPartition.java
@@ -1,32 +1,43 @@
 package com.quest.exams.subquestions;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListPartition {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arrayList=new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(3);
-        arrayList.add(3);
-        int partitionSize = 2;
-        List<List<Integer>> resultList = partitionList(arrayList, partitionSize); //stores the list of lists
+        System.out.println("Enter the number of elements in the list:");
+        int numberOfElements = sc.nextInt();
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < numberOfElements; i++) {
+            arrayList.add(sc.nextInt());
+        }
+
+        System.out.println("Enter the sublist size");
+        int subListSize = sc.nextInt(); // indicate sublist size
+
+        System.out.println("The input array list");
+        System.out.println(arrayList);
+
+        List<List<Integer>> resultList = partitionList(arrayList, subListSize); //stores the list of lists
+
+        System.out.println("The resultant list is:");
         System.out.println(resultList);
     }
-    public static List<List<Integer>> partitionList(List<Integer> inputList, int n) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> currentPartition = new ArrayList<>(); // initially creates inner list
+    public static List<List<Integer>> partitionList(List<Integer> inputList, int subListSize) {
+        List<List<Integer>> resultList =new ArrayList<>(); // list to store the result
+        List<Integer> currentPartition =new ArrayList<>(); // initially creates inner list
 
         for (int i = 0; i < inputList.size(); i++) {
             currentPartition.add(inputList.get(i));
 
-            if (currentPartition.size() == n || i == inputList.size() - 1) {
-                result.add(currentPartition);
+            if (currentPartition.size()==subListSize || i==inputList.size()-1) {
+                resultList.add(currentPartition);
                 currentPartition = new ArrayList<>();  // created new inner list for next entry
             }
         }
-        return result;
+        return resultList;
     }
 }
